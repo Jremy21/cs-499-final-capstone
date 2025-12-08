05240f72b0432bf9d1fe2a800faaa97949905c50
jremy21 <jermbob1999@gmail.com>
2025-12-07 23:49:52 -0500

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -4,43 +4,51 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Server-side routers
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+
+// API router
+var apiRouter = require('./app_api/routes/index');
+
+// Handlebars
 var handlebars = require('hbs');
+
+// Connect to the database
+require('./app_api/models/db');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-
-// register handlebars partials
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
-
 app.set('view engine', 'hbs');
 
+// middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ROUTES
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error =
+    req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,14 +1,49 @@
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./app_server/data/trips.json', 'utf8'));
+// API endpoint for retrieving trips data
+const tripsEndpoint = "http://localhost:3000/api/trips";
 
-/* GET travel view */
-const travel = (req, res) => {
-    res.render('travel', { 
-        title: 'Travlr Getaways',
-        trips
-    });
+// Options used with fetch for GET request
+const options = {
+    method: "GET",
+    headers: {
+        "Accept": "application/json"
+    }
 };
 
+// var fs = require("fs");
+// var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
+
+/* GET travel view */
+const travel = async function (req, res, next) {
+
+    await fetch(tripsEndpoint, options)
+        .then(res => res.json())
+        .then(json => {
+
+            let message = null;
+
+            // Check if response is not an array
+            if (!(json instanceof Array)) {
+                message = "API lookup error";
+                json = [];  // Reset json to empty list
+            }
+            else {
+                // Check if array is empty
+                if (json.length === 0) {
+                    message = "No trips exist in our database";
+                }
+            }
+
+            // Render travel view
+            res.render("travel", {
+                title: "Travlr Getaways",
+                trips: json,
+                message
+            });
+        })
+        .catch(err => res.status(500).send(err.message));
+};
+
+// Export controller method
 module.exports = {
-    travel
+    travel,
 };
